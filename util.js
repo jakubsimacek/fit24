c62736b7f0b64fac95a9b07c3367896ae86b1933
Jakub Šimáček <jakubsimacek@email.cz>
2017-10-16 07:35:43 +0200

    new time calculation

--- util.js
+++ util.js
@@ -11,6 +11,7 @@ module.exports.renderr = function (res, message, error, data) {
 
 
 // converts string "14:45" into 14.45, ...
+/*
 let timeToDec = function (timeStr) {
   const tParts = timeStr.split(":");
   const hh = Number(tParts[0]);
@@ -18,10 +19,15 @@ let timeToDec = function (timeStr) {
   return Number(hh + (mm/100)).toFixed(2);
 }
 module.exports.timeToDec = timeToDec;
+*/
+let timeToDate = function (timeStr) {
+  return new Date('1970-01-01T' + baseTime + ':00.000Z')
+}
+module.exports.timeToDec = timeToDec;
 
 // function adds the specified number of minutes to baseTime
 let addToTime = function (baseTime, minutes) {
-  const baseHours = Math.floor(baseTime);
+/*  const baseHours = Math.floor(baseTime);
   const baseMins = (baseTime - baseHours) * 100;
   let addHours = Math.floor(minutes / 60);
   const addMins = minutes - addHours * 60;
@@ -32,12 +38,16 @@ let addToTime = function (baseTime, minutes) {
     addHours += 1;
   }
   return baseHours + addHours + (intermAdd / 100);
+*/
+  const d = new Date(baseTime)
+  d.setUTCMinutes(d.getUTCMinutes() + minutes)
+  return d
 }
 module.exports.addToTime = addToTime;
 
 // function subtracts the specified number of minutes from baseTime
 let subFromTime = function (baseTime, minutes) {
-  const baseHours = Math.floor(baseTime);
+/*  const baseHours = Math.floor(baseTime);
   const baseMins = (baseTime - baseHours) * 100;
   let subtractHours = Math.floor(minutes / 60);
   const subtractMins = minutes - subtractHours * 60;
@@ -49,9 +59,43 @@ let subFromTime = function (baseTime, minutes) {
   }
 //  return Math.round(baseHours - subtractHours - (intermSub / 100), -2);
   return Number(baseHours - subtractHours + (intermSub / 100)).toFixed(2);
+*/
+  return addToDate(baseTime, -minutes)
 }
 module.exports.subFromTime = subFromTime;
 
+module.exports.makeTimeOccupation = function (week) {
+  let arrFree = [ { start : '04.00', end : '22.00' }];
+//console.log('top');
+  week.days.map(d => { //console.log('d: ' + CircularJSON.stringify(d));
+    d.terms.map(t => { //console.log('t: ' + CircularJSON.stringify(t));
+      const start = subFromTime(timeToDate(t.start), timePadding);
+      const end = (t.end) ? addToTime(timeToDate(t.end), timePadding) : addToTime(start, defaultDuration + 2*timePadding);
+console.log('start: ' + start + ', end: ' + end);
+      const tempMap = arrFree.map(free => {console.log('free: ' + CircularJSON.stringify(free));
+        // narrowing free interval from left
+        if (free.start <= end && free.start >= start){console.log('1: ');
+          return { start : end, end : free.end };}
+        // narrowing interval from right
+        else if (free.end >= start && free.end <= end){console.log('2: ');
+          return { start : free.start, end : start };}
+        // splitting interval
+        else if (free.start < start && free.end > end){console.log('3: ');
+          return [{ start : free.start, end : start}, 
+                  { start : end, end : free.end }];}
+        return { start : free.start, end : free.end };
+      });
+  //    console.log(CircularJSON.stringify(tempMap));
+      arrFree = [].concat.apply([], tempMap).filter(free => {
+        return free.end > addToTime(free.start, 60); // free interval valid and at least +1 hour wide
+      });
+    });
+  });
+  //console.log('ret: ' + CircularJSON.stringify(arrFree));
+  return arrFree;
+}
+
+/*
 module.exports.makeTimeOccupation = function (week) {
   let arrFree = [ { start : 4.00, end : 22.00 }];
 //console.log('top');
@@ -82,7 +126,7 @@ console.log('start: ' + start + ', end: ' + end);
   //console.log('ret: ' + CircularJSON.stringify(arrFree));
   return arrFree;
 }
-
+*/
 module.exports.add = function (a, b) {
   return a + b;
 }
